0863a7c820c25acca5c014e55eddb1a1e336e000
rleonard21 <bl.cirrus@gmail.com>
2019-09-09 12:29:47 -0400

    done

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+import random
+
+print("Robert Leonard")
+
+one = random.randrange(0,100,1)
+two = random.randrange(0,100,1)
+
+print(one)
+print(two)
+print("Sum = {}".format(one + two))
+print("Average = {}".format((one + two)/2.0))
